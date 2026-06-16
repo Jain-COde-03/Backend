6f7f5930f01082b0c6c5637885bd50e88bfdbcf8
Jain-COde-03 <jaindhairya002@gmail.com>
2026-06-16 23:42:58 +0530

    MongoDB Connected

---
+++ src/server.js
@@ -0,0 +1,33 @@
+// require("dotenv").config({path : './env'})
+import dotenv from "dotenv" ;
+import express from "express" ;
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+const app = express() ;
+
+connectDB() ;
+
+/*
+(async() => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`) 
+        app.on("error",(error)=>{
+            console.log("Err",error);
+            throw error 
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is LIstening on ${process.env.PORT}`);
+            
+        })
+    }catch(error){
+        console.log("Error",error);
+        throw err ;
+    }
+})
+*/
+
